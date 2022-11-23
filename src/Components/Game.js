593065bcce840a2d2938e604dbe29c208039f972
Mikael Josias <mikaeljosias2002@gmail.com>
2022-11-23 14:32:36 -0300

    style(style.css): create basic style and css variables

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,10 +1,10 @@
 export default function Game() {
-  return (
-    <div>
-      <button>Escolher Palavra</button>
-      <img src="./assets/imgs/forca0.png" />
-      <span>_ _ _ _ _ _ _ _ _</span>
-    </div>
-    );
-  }
-  
+	return (
+		<div>
+			<button>Escolher Palavra</button>
+			<img src="./assets/imgs/forca0.png" />
+			<span>_ _ _ _ _ _ _ _ _</span>
+		</div>
+		);
+	}
+	
