422dba45b9539b6c83740510738fb0a1c10848e4
Mikael Josias <mikaeljosias2002@gmail.com>
2022-11-24 17:14:09 -0300

    feat(Letter.js): added disabled property to keyboard letter

--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Letter from "./Letter";
 
 export default function Keyboard(){
+
+    let [activated, setActivated] = useState(true);
 
     const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     return (
         <div className="Keyboard deactivated">
-            {letters.map(l => <Letter letter={l} />)}
+            {letters.map(l => <Letter letter={l} disabled={activated} />)}
         </div>
     );
 }
--- src/Components/Letter.js
+++ src/Components/Letter.js
@@ -1,7 +1,12 @@
-export default function Letter({letter}) {
+export default function Letter({letter, disabled}) {
+
+    function ola(){
+        console.log("ola");
+    }
+
     return (
         <>
-            <button className="Keyboard__letter" value={letter}>{letter}</button>
+            <button className="Keyboard__letter" value={letter} disabled={disabled} onClick={ola} >{letter}</button>
         </>
     );
 }
