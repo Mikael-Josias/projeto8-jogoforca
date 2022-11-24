1f956a2136bd38d8caa11a211338a92e9aa3e835
Mikael Josias <mikaeljosias2002@gmail.com>
2022-11-24 16:00:15 -0300

    feat(Guess.js): added guess section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Game from "./Components/Game";
+import Guess from "./Components/Guess";
 import Keyboard from "./Components/Keyboard";
 
 function App() {
@@ -6,9 +7,9 @@ function App() {
 		<div className="App">
 			<Game />
 			<Keyboard/>
+			<Guess/>
 		</div>
-		);
-	}
-	
-	export default App;
-	
+	);
+}
+
+export default App;
---
+++ src/Components/Guess.js
@@ -0,0 +1,9 @@
+export default function Guess() {
+    return (
+        <div className="Guess">
+            <span>Já sei a palavra!</span>
+            <input type="text" className="Guess__input"/>
+            <button className="Guess__button">Chutar</button>
+        </div>
+    );
+}
