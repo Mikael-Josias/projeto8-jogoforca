0f688ea1403a3ff13006f90d532df1220dc81c2a
Mikael Josias <mikaeljosias2002@gmail.com>
2022-11-24 16:32:52 -0300

    style(keyboard.css): deactivated letters

--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -5,7 +5,7 @@ export default function Keyboard(){
     const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     return (
-        <div className="Keyboard">
+        <div className="Keyboard deactivated">
             {letters.map(l => <Letter letter={l} />)}
         </div>
     );
